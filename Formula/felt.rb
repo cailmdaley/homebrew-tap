ccc6d6c0094b670ea3fc8127e4a15663ae569842
Cail Daley <cailmdaley@gmail.com>
2026-02-22 06:41:11 +0100

    felt 0.2.0

--- Formula/felt.rb
+++ Formula/felt.rb
@@ -4,28 +4,28 @@
 class Felt < Formula
   desc "DAG-native task tracker. Markdown files with dependencies."
   homepage "https://github.com/cailmdaley/felt"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/cailmdaley/felt/releases/download/v0.1.0/felt_Darwin_x86_64.tar.gz"
-      sha256 "3d630e258a72eae4c33562d4dc3889ac9cee2ba57f016dfe660f66a35ccc60a2"
+      url "https://github.com/cailmdaley/felt/releases/download/v0.2.0/felt_Darwin_x86_64.tar.gz"
+      sha256 "604b172a5f78cd94003bcaf067ad58d04f1dac82749ef0d0e46bbd4b7ae874c1"
     end
     on_arm do
-      url "https://github.com/cailmdaley/felt/releases/download/v0.1.0/felt_Darwin_arm64.tar.gz"
-      sha256 "d9d71751c6c775d1963100605f138dba14764951c78a4bc6b7aa9e8ad16a8df5"
+      url "https://github.com/cailmdaley/felt/releases/download/v0.2.0/felt_Darwin_arm64.tar.gz"
+      sha256 "fddfb551f56ba52cd8cac5f9a1ccce3a980e1d8f21696fe1baab58dc2f0a14fe"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/cailmdaley/felt/releases/download/v0.1.0/felt_Linux_x86_64.tar.gz"
-      sha256 "29c6fc53c597e252b752fe03ee470ec29c3768a1b26bc55aaf812caf3a2d9781"
+      url "https://github.com/cailmdaley/felt/releases/download/v0.2.0/felt_Linux_x86_64.tar.gz"
+      sha256 "1488b9f90673e984db8673e2459fd3cc145436cc676879d65644df02a47aaeca"
     end
     on_arm do
-      url "https://github.com/cailmdaley/felt/releases/download/v0.1.0/felt_Linux_arm64.tar.gz"
-      sha256 "2744cb9f9da45b117fa58ae8b761159341a254daa9b430a6d3baa4d57ae4e609"
+      url "https://github.com/cailmdaley/felt/releases/download/v0.2.0/felt_Linux_arm64.tar.gz"
+      sha256 "c62407ecfd17615d2d2391c19aa82e3f1710302fd9c790a665eb7de4549eb981"
     end
   end
 
